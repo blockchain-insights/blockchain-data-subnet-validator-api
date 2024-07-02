41f0e4523a298c4ee828f6408b51a678ca9f6c2f
Darek ADO <Dariusz.Bzowka@chain-insights.ai>
2024-07-02 07:19:57 +0000

    from ADO

--- scripts/run_validator_api.sh
+++ scripts/run_validator_api.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+# Check if the api_key.json file exists and create a symlink if it does
+if [ -f /app/config/api_key.json ]; then
+  ln -s /app/config/api_key.json /app/api_key.json
+fi
+
+# Check if the rate_limit.json file exists and create a symlink if it does
+if [ -f /app/config/rate_limit.json ]; then
+  ln -s /app/config/rate_limit.json /app/rate_limit.json
+fi
+
 cd "$(dirname "$0")/../"
 export PYTHONPATH=$PWD
 python3 main.py --wallet.name "$WALLET_NAME" --wallet.hotkey "$WALLET_HOTKEY" --netuid "$NETUID" --subtensor.network "$SUBTENSOR_NETWORK" --subtensor.chain_endpoint "$SUBTENSOR_URL" --logging.trace
